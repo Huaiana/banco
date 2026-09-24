22ddd61cb338209820f165982c913c569df07a9a
huaydarc@gmail.com <huaydarc@gmail.com>
2026-09-23 21:30:06 -0300

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	new file:   src/main/java/com/git/banco/Service/CartaoService.java #	new file:   src/main/java/com/git/banco/Service/ClienteService.java

---
+++ src/main/java/com/git/banco/Service/ClienteService.java
@@ -0,0 +1,32 @@
+package com.git.banco.Service;
+
+import com.git.banco.model.Cliente;
+import com.git.banco.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service 
+public class ClienteService {
+    
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> getClienteById(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente saveCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public void deleteCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
